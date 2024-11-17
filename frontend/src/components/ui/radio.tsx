4da9682d0f3108a8a590fc7558ea82ab63690a77
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2024-11-16 21:36:29 -0300

    style: updated styles form

--- frontend/src/components/ui/radio.tsx
+++ frontend/src/components/ui/radio.tsx
@@ -5,17 +5,18 @@ import { FieldError } from 'react-hook-form';
 interface IRadioProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: FieldError | undefined;
+  name?: string;
 }
 
 const Radio = forwardRef<HTMLInputElement, IRadioProps>(
-  ({ label, error, ...rest }, ref) => {
+  ({ label, error, name, ...rest }, ref) => {
     return (
       <div className="flex items-center gap-2 mt-2">
         <input
           type="radio"
           id={label}
-          name={label}
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 "
+          name={name}
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           ref={ref}
           {...rest}
         />
