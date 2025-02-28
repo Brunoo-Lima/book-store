3ced3c0451998d0e67b5af412d365a48607dc0eb
Danilo de Araújo Leite Gomes <112290478+JayXani@users.noreply.github.com>
2025-02-27 21:50:11 -0300

    update database model

--- frontend/src/components/protected-route/protected-route.tsx
+++ frontend/src/components/protected-route/protected-route.tsx
@@ -5,16 +5,5 @@ import { PropsWithChildren, useEffect } from 'react';
 export const ProtectedRoute = ({ children }: PropsWithChildren) => {
   const { isAuthenticated, loading } = useAuth();
   const router = useRouter();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.push('/');
-    }
-  }, [isAuthenticated, loading, router]);
-
-  if (loading || !isAuthenticated) {
-    return <p>Carregando...</p>;
-  }
-
   return children;
 };
